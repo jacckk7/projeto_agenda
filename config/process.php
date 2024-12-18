14c09ecd1a91b6901b147878e3a9cee586df954c
jacckk7 <brenocal73@gmail.com>
2024-12-18 09:43:03 -0300

    feat: Feita a tabela para mostrar os dados na home.

--- config/process.php
+++ config/process.php
@@ -5,6 +5,8 @@
     include_once("config/conection.php");
     include_once("config/url.php");
 
+    $contacts = [];
+
     $query = "SELECT * FROM contacts";
 
     $stmt = $conn->prepare($query);
